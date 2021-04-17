aee248cef35011afc388fd6abf2cecc316f3dc4f
Jonas Hübotter <jonas.huebotter@gmail.com>
2021-04-17 22:06:05 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 # Setup documents
 
 # Generate a handout
-cp ./slides/en.tex ./slides/handout-1-en.tex
+cp ./slides/1-en.tex ./slides/handout-1-en.tex
 sed -i "1s/.*/\\\\documentclass\[handout\]\{beamer\}/" ./slides/handout-1-en.tex
-cp ./slides/de.tex ./slides/handout-1-de.tex
+cp ./slides/1-de.tex ./slides/handout-1-de.tex
 sed -i "1s/.*/\\\\documentclass\[handout\]\{beamer\}/" ./slides/handout-1-de.tex
